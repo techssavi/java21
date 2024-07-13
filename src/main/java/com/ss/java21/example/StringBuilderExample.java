e4b30cde0d3416cc166d3d73994d5205364e0129
savit <savitha.satyanarayan@gmail.com>
2024-07-13 17:13:49 +0530

    StringBuilder Example

---
+++ src/main/java/com/ss/java21/example/StringBuilderExample.java
@@ -0,0 +1,12 @@
+package com.ss.java21.example;
+
+public class StringBuilderExample {
+
+
+    public static void main(String[] args){
+        StringBuilder builder =new StringBuilder("This is an example of string repeat");
+
+        builder.repeat("*", 10);
+        System.out.println(STR."Final response - \{builder}");
+    }
+}
