6160fb2d636989aa091b136d76ab20a690ed792e
savit <savitha.satyanarayan@gmail.com>
2024-07-13 20:54:44 +0530

    StringBuilder Example

---
+++ src/main/java/com/ss/java21/example/ScopedValueExample.java
@@ -0,0 +1,19 @@
+package com.ss.java21.example;
+
+public class ScopedValueExample {
+
+    static ScopedValue<String> SCOPED_USEROBJ = ScopedValue.newInstance();
+
+    public static void main(String[] args) {
+       ScopedValue.runWhere(SCOPED_USEROBJ, args[0], () -> {
+            System.out.println("User: " + SCOPED_USEROBJ.get());
+            processUserData();
+        });
+        ;
+
+    }
+
+    private static void processUserData() {
+        System.out.println("Processing data for: " + SCOPED_USEROBJ.get() );
+    }
+}
