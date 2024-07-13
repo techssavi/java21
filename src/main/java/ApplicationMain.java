974291b2fe9f3184b5508f08d75c3745db73a985
savit <savitha.satyanarayan@gmail.com>
2024-07-13 16:40:52 +0530

    Initial Changes

---
+++ src/main/java/ApplicationMain.java
@@ -0,0 +1,10 @@
+package com.ss.java21.example;
+
+
+public class ApplicationMain {
+
+    public static void main(String[] args){
+        System.out.println ("Here in application main");
+    }
+
+}
